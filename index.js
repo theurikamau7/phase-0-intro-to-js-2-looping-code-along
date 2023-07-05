61d60ad94d6090c9dc74bfb26eaa420b6dc59f80
Iankamau1 <theurikamau7@gmail.com>
2023-07-05 13:57:54 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+
+const writeCards = function(names, event){
+    let giftMsgs = [];
+    for (let i = 0; i < names.length; i++){
+        let giftMsg = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        giftMsgs.push(giftMsg)
+    }
+
+    return giftMsgs;
+}
+writeCards(names, "surprise")
+
+const countDown = function(n){
+    let i = n;
+    while(i >= 0){
+        console.log(i);
+        --i
+    }
+
+}
+
+countDown(10)
